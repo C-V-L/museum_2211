a2b96787ea15b886d3eab68569d2324566b0262a
Conner Van Loan <cdvanloan@gmail.com>
2023-01-03 09:07:45 -0700

    Initial commit + exhibit #exists test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,10 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+  let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0})}
+  describe '#initialize' do
+    it 'exists' do
+      expect(exhibit).to be_a(Exhibit)
+    end
+  end
 end
