46f070867eac091c2754fc5e5e178058ec64ad94
Conner Van Loan <cdvanloan@gmail.com>
2023-01-03 09:18:21 -0700

    Adds #add_interest test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,6 +11,15 @@ RSpec.describe Patron do
     it 'has attributes' do
       expect(patron_1.name).to eq("Bob")
       expect(patron_1.spending_money).to eq(20)
+      expect(patron_1.interests).to eq([])
+    end
+  end
+
+  describe '#add_interest' do
+    it 'can add an interest' do 
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
     end
   end
 end
